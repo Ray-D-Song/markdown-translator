873b9bceba6fcbe24b78db2a8b2fef3be464c8f3
Ray <songraysmail@gmail.com>
2024-08-12 22:08:09 +0800

    delete: remove unnecessary files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { defineExtension } from 'reactive-vscode'
+import { defineExtension, useCommand } from 'reactive-vscode'
 import { window } from 'vscode'
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+  useCommand('markdown translator.translate', () => {
+    window.showInformationMessage('Hello World!')
+  })
 })
 
 export { activate, deactivate }
